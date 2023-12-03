a31efff909acfbd8126fa2d8c0283ff14b2ce4ca
xueqili02 <xueqili00@gmail.com>
2023-12-03 22:38:15 +0800

    update

--- source/js/google_analytics.js
+++ source/js/google_analytics.js
@@ -1,5 +1,10 @@
-var script = document.createElement("script");
-script.src = "/google_script.js";
-
+var script1 = document.createElement("script");
+script1.src = "https://www.googletagmanager.com/gtag/js?id=G-W9XDVER7YY";
+script1.async = true;
 var head = document.getElementsByName("head")[0];
-head.appendChild(script);
+head.appendChild(script1);
+
+var script2 = document.createElement("script");
+script2.src = "/google_script.js";
+
+head.appendChild(script2);
